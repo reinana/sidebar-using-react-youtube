0d9d1aa439d64059b9995830a57d132b28b6dcf0
reinana <reina.ceil.0510@gmail.com>
2023-08-12 09:33:47 +0900

    first commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { SidebarData } from './SidebarData'
+import SidebarIcon from './SidebarIcon'
+
+const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+            <SidebarIcon />
+            <ul className='sidebarList'>
+                {SidebarData.map((value, key) => {
+                    return (
+                        <li
+                            key={key}
+                            id={window.location.pathname == value.link ? "active" : ""}
+                            className='row'
+                            onClick={() => {
+                                window.location.pathname = value.link
+                            }}
+                        >
+                            <div id='icon'>{value.icon}</div>
+                            <div id='title'>{value.title}</div>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ src/components/SidebarIcon.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Icon from "../images/1317915.png";
+
+const SidebarIcon = () => {
+  return (
+    <div className='sidebarIcon'>
+        <img src={Icon} alt=''/>
+        <p>programing@gmail.com</p>
+    </div>
+  )
+}
+
+export default SidebarIcon
